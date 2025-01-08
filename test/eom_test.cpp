e2d15f1e1b36e3654f362c07984f0513044108d3
ytokumaru <ytokumaru16@gmail.com>
2025-01-08 17:40:09 +0900

    add eom test

--- test/eom_test.cpp
+++ test/eom_test.cpp
@@ -99,6 +99,64 @@ TEST(EOMTest, Repulsion)
   EXPECT_DOUBLE_EQ(delta_v_2.z, 0);
 }
 
+TEST(EOMTest, RepulsionOverBoundary)
+{
+  // Place two fish in the vicinity of each other and check the distance
+  // Note that the distance between the two is less than the repulsion radius
+  // Also note that the movement of the fish is confined to the xy plane
+  Fish fish1({ 9.75, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 }, 0);
+  Fish fish2({ 0.25, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 }, 0);
+
+  SimParam sim_param{ .length = 10, .max_steps = 100, .n_fish = 2 };
+
+  FishParam fish_param{ .vel_standard = 1.0,
+    .vel_repulsion = 1.0,
+    .vel_escape = 7.5,
+    .body_length = 1.0,
+    .repulsion_radius = 0.5,
+    .attraction_radius = 7.5,
+    .n_cog = 3,
+    .attraction_str = 10.0,
+    .attraction_duration = 0.1 };
+
+  // Create dummy cells
+  std::vector<std::vector<std::vector<std::vector<Fish *>>>> cells(sim_param.length,
+    std::vector<std::vector<std::vector<Fish *>>>(
+      sim_param.length, std::vector<std::vector<Fish *>>(sim_param.length)));
+  cells[9][0][0].push_back(&fish1);
+  cells[0][0][0].push_back(&fish2);
+
+  auto inner = getInnerCells(fish_param.repulsion_radius);
+  auto boundary = getBoundaryCells(fish_param.repulsion_radius);
+
+  EXPECT_EQ(inner.size(), 0);
+  EXPECT_THAT(boundary, UnorderedElementsAreArray(std::vector<std::array<int, 3>>({ { 0, 0, 0 } })));
+
+  auto [delta_v_1, n_fish_1] = calcRepulsion(fish1, sim_param, fish_param, cells, boundary, inner);
+  EXPECT_EQ(n_fish_1, 1);
+  fish1.setDeltaVelocity(delta_v_1);
+
+  auto [delta_v_2, n_fish_2] = calcRepulsion(fish2, sim_param, fish_param, cells, boundary, inner);
+  EXPECT_EQ(n_fish_2, 1);
+  fish2.setDeltaVelocity(delta_v_2);
+
+  EXPECT_DOUBLE_EQ(delta_v_1.x, -2);
+  EXPECT_DOUBLE_EQ(delta_v_1.y, -2);
+  EXPECT_DOUBLE_EQ(delta_v_1.z, 0);
+
+  EXPECT_DOUBLE_EQ(delta_v_2.x, 2);
+  EXPECT_DOUBLE_EQ(delta_v_2.y, -2);
+  EXPECT_DOUBLE_EQ(delta_v_2.z, 0);
+
+  // The delta velocities should be equal and opposite
+  EXPECT_DOUBLE_EQ(delta_v_1.x, -delta_v_2.x);
+  EXPECT_TRUE(delta_v_1.x < 0);
+  EXPECT_TRUE(delta_v_2.x > 0);
+  EXPECT_DOUBLE_EQ(delta_v_1.y, delta_v_2.y);
+  EXPECT_DOUBLE_EQ(delta_v_1.z, 0);
+  EXPECT_DOUBLE_EQ(delta_v_2.z, 0);
+}
+
 TEST(EOMTest, Attraction)
 {
   // Place two fish in the vicinity of each other and check the distance
